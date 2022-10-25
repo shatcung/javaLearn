68699b5dc6d4e486b0fbcd3cae332dd5485c2e5d
shatcung <shatcung@ya.ru>
2022-10-25 04:01:50 +0300

    Clean up conditions branch

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    Dog ourdog = new Dog();
-    ourdog.walk();
-    ourdog.plays();
-    String say = ourdog.barking("Play with me ");
-    System.out.println(say);
-  }
-}
