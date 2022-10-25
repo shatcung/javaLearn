1e365ff6bb42ffd822c763416202514cd0246987
shatcung <shatcung@ya.ru>
2022-10-25 03:58:21 +0300

    Clean Up arrays branch

--- Other/To_the_power.java
+++
@@ -1,8 +0,0 @@
-public class To_the_power {
-  public static void main(String[] args)   {
-    int n = 3, x = 5, r = 1;
-    for (int i = 0; i < n; i++)
-      r *= x;
-    System.out.print(r + " ");
-  }
-}
